a561824cdf9fc40a6c41ee6deb15c6b7c2d1decc
Zachary Hawkhead <zachary.hawkhead@ymail.com>
2022-04-27 11:55:27 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages
+from setuptools import setup,Extension
+
+setup(name="bib2doi",
+      version="1.0.0",
+      packages=find_packages(),
+      description="Package for findind DOI for bibtex bibliography",
+      url="https://github.com/zachary-hawk/bib2doi.git",
+      author="Zachary Hawkhead",
+      author_email="zachary.hawkhead@ymail.com",
+      license="MIT",
+      install_requires=["numpy",
+                        "Levenshtein",
+                        "bibtexparser",
+                        "argparse",
+                        "habanero"],
+
+      entry_points={"console_scripts":["bib2doi=src.main:main_bib",]
+                    }
+
+      )
+
+
